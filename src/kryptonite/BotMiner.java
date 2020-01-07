748890159b0d520e3bd891a8458ef075b895199a
rzhan11 <richymanzhan@gmail.com>
2020-01-06 23:48:41 -0500

    miners explore directions and rotate around obstacles

--- src/kryptonite/BotMiner.java
+++ src/kryptonite/BotMiner.java
@@ -4,12 +4,44 @@ import battlecode.common.*;
 
 public class BotMiner extends Globals {
 
+	private static boolean firstTurn = true;
+
+	private static MapLocation HQLocation;
+
+	// Miner moves in the direction that it was spawned in, until it sees a soup deposit or hits a map edge
+	// If it hits a map edge, it will rotate left the currentExploringDirection
+
+	private static Direction currentExploringDirection;
+
+	private static MapLocation locatedSoupDeposit = null;
+
 	public static void loop() throws GameActionException {
+
 		while (true) {
 			int startTurn = rc.getRoundNum();
 			try {
 				Globals.update();
+				Globals.updateRobot();
+
+				// Do first turn code here
+				if (firstTurn) {
+					// identify HQ MapLocation
+					for (RobotInfo ri: visibleAllies) {
+						if (ri.team == us && ri.type == RobotType.HQ) {
+							HQLocation = ri.location;
+						}
+					}
+					if (HQLocation == null) {
+						System.out.println("ERROR: Failed sanity check - Cannot find HQLocation");
+					} else {
+						System.out.println("HQ is located at " + HQLocation);
+					}
+					currentExploringDirection = HQLocation.directionTo(here);
+					System.out.println("Exploring " + currentExploringDirection);
+				}
+
 			    turn();
+				firstTurn = false;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -22,6 +54,52 @@ public class BotMiner extends Globals {
 	}
 
 	public static void turn() throws GameActionException {
+		/*
+		if I have already found a soup deposit or I see a soup deposit
+			flag this event and bug-nav towards it
+		else
+			move in the currentExploringDirection if possible
+			if I encounter a map edge, rotateLeft the currentExploringDirection
+			if I encounter a water/elevation obstacle, bug-nav around it
+		*/
 
+		if (locatedSoupDeposit != null || locateSoupDeposit()) {
+			System.out.println("Found soup at " + locatedSoupDeposit);
+		} else {
+			if (rc.isReady()) {
+				int count = 0;
+				Direction curDir = currentExploringDirection;
+				while (!rc.canMove(curDir) && count < 8) {
+					curDir = curDir.rotateLeft();
+					count++;
+				}
+				if (count < 8) {
+					rc.move(curDir);
+				}
+			}
+		}
+	}
+
+	/*
+	To be implemented
+	*/
+	public static void bugNavigate (MapLocation target) throws GameActionException {
+
+
+	}
+
+	/*
+	Returns false if no soup deposit was found
+	Returns true if soup deposit was found and also saves the MapLocation in variable 'locatedSoupDeposit'
+	*/
+	public static boolean locateSoupDeposit() throws GameActionException {
+		for (int[] pair: sensableDirections) {
+			MapLocation loc = new MapLocation(here.x + pair[0], here.y + pair[1]);
+			if (rc.senseSoup(loc) >= 0) {
+				locatedSoupDeposit = loc;
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/kryptonite/Globals.java
+++ src/kryptonite/Globals.java
@@ -14,20 +14,10 @@ public class Globals {
 	public static RobotType myType;
 	public static int mySensorRadiusSquared;
 
+	public static int[][] sensableDirections = null; // stores (dx, dy) of locations that can be sensed
+
 	public static Direction[] directions = {Direction.NORTH, Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST};
-
 	public static Direction[] diagonalDirections = {Direction.NORTHEAST, Direction.SOUTHEAST, Direction.SOUTHWEST, Direction.NORTHWEST};
-
-	public static void init(RobotController theRC) {
-		rc = theRC;
-
-		us = rc.getTeam();
-		them = us.opponent();
-
-		myID = rc.getID();
-		myType = rc.getType();
-		mySensorRadiusSquared = myType.sensorRadiusSquared;
-	}
 
 	/*
 	Values that might change each turn
@@ -37,9 +27,57 @@ public class Globals {
 	public static int roundNum;
 	public static int teamSoup;
 
-	public static void update() {
+	public static RobotInfo[] visibleAllies = null;
+
+	public static void init(RobotController theRC) {
+		rc = theRC;
 		here = rc.getLocation();
+
+		us = rc.getTeam();
+		them = us.opponent();
+
+		myID = rc.getID();
+		myType = rc.getType();
+		mySensorRadiusSquared = myType.sensorRadiusSquared;
+		sensableDirections = calculateSensableDirections(mySensorRadiusSquared);
+	}
+
+	public static void update() {
 		roundNum = rc.getRoundNum();
 		teamSoup = rc.getTeamSoup();
+		visibleAllies = rc.senseNearbyRobots(mySensorRadiusSquared, us);
+	}
+
+	public static void updateRobot() {
+		here = rc.getLocation();
+	}
+
+	public static int[][] calculateSensableDirections(int sensorRadiusSquared) {
+		// Temporary debug
+		return new int[0][2];
+		// int maxWidth = 7; // 6 + 1 to be safe (for HQ)
+		//
+		// int size = 0;
+		// for (int dx = -maxWidth; dx <= maxWidth; dx++) {
+		// 	for (int dy = -maxWidth; dy <= maxWidth; dy++) {
+		// 		if (sensorRadiusSquared >= dx * dx + dy * dy) {
+		// 			size++;
+		// 		}
+		// 	}
+		// }
+		// int[][] temp = new int[size][2];
+		//
+		// int index = 0;
+		// for (int dx = -maxWidth; dx <= maxWidth; dx++) {
+		// 	for (int dy = -maxWidth; dy <= maxWidth; dy++) {
+		// 		if (sensorRadiusSquared >= dx * dx + dy * dy) {
+		// 			temp[index][0] = dx;
+		// 			temp[index][1] = dy;
+		// 			System.out.println("dx, dy " + dx + " " + dy);
+		// 			index++;
+		// 		}
+		// 	}
+		// }
+		// return temp;
 	}
 }
