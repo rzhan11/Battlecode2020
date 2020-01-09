910985db4f08f22d6de2c10d4f1fe236d94affdd
Senpat <pgz11902@gmail.com>
2020-01-08 21:50:12 -0500

    add netgun

--- src/kryptonite/BotNetGun.java
+++ src/kryptonite/BotNetGun.java
@@ -8,7 +8,7 @@ public class BotNetGun extends Globals {
 		while (true) {
 			try {
 				Globals.update();
-			    turn();
+				turn();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -17,6 +17,30 @@ public class BotNetGun extends Globals {
 	}
 
 	public static void turn() throws GameActionException {
+
+		while(rc.isReady()) {
+			RobotInfo[] robots = rc.senseNearbyRobots();
+			int mind = Integer.MAX_VALUE;
+			int id = -1;
+			//MapLocation here = rc.getLocation();
+
+			for (RobotInfo ri : robots) {
+				if (ri.team == rc.getTeam().opponent() && ri.type == RobotType.DELIVERY_DRONE){
+					int curdis = here.distanceSquaredTo(ri.location);
+					if(curdis < mind){
+						mind = curdis;
+						id = ri.ID;
+					}
+				}
+			}
+
+			if(id!=-1){
+				rc.shootUnit(id);
+			}
+
+
+		}
+
 
 	}
 }
