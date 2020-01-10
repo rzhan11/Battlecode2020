d347bd28fe55e5e77522c9b2af102a2f760cc93f
Havish Malladi <havishmalladi@gmail.com>
2020-01-10 14:12:59 -0500

    updated file

--- src/kryptonite/BotDeliveryDrone.java
+++ src/kryptonite/BotDeliveryDrone.java
@@ -4,12 +4,14 @@ import battlecode.common.*;
 
 public class BotDeliveryDrone extends Globals {
 
+	public static int smallWallDepth;
+
 	public static void loop() throws GameActionException {
 		while (true) {
 			try {
 				Globals.update();
 				if (firstTurn) {
-
+					smallWallDepth = rc.senseElevation(HQLocation);
 				}
 			    turn();
 			} catch (Exception e) {
@@ -22,8 +24,58 @@ public class BotDeliveryDrone extends Globals {
 	public static boolean canPickUp (RobotType rt) {
 		return rt == RobotType.MINER || rt == RobotType.LANDSCAPER || rt == RobotType.COW;
 	}
-    // TODO: Be Better.
+    // TODO: Identify if we are one a low elevation, pick up robots here, and move to clean space
 	public static void turn() throws GameActionException {
+		if (rc.isCurrentlyHoldingUnit()) {
+			//move to adjacent square that is higher elevation
+			for (Direction dir : directions) {
+				MapLocation loc = rc.adjacentLocation(dir);
+				if (inMap(loc) && rc.senseElevation(loc) >= smallWallDepth && rc.canDropUnit(dir)) {
+					rc.dropUnit(dir);
+					Debug.tlog("Put robot on the wall at " +  loc);
+					return;
+				}
+			}
+		} else {
+			//check for adjacent robots
+			for (Direction dir: directions) {
+				MapLocation loc = rc.adjacentLocation(dir);
+				if (inMap(loc)) {
+					RobotInfo ri = rc.senseRobotAtLocation(loc);
+					if (ri != null && ri.team == us && rc.canPickUpUnit(ri.ID) && rc.senseElevation(loc) < smallWallDepth-3) {
+						rc.pickUpUnit(ri.ID);
+						Debug.tlog("Picked up unit at " + loc);
+						return;
+					}
+				}
+			}
+
+			//find closet robot that is missing
+			int closestAllyDist = P_INF;
+			int closestAllyIndex = -1;
+			int index = 0;
+			for (RobotInfo ri: visibleAllies) {
+				if (canPickUp(ri.type) && rc.senseElevation(ri.location) < smallWallDepth-3) {
+					int dist = here.distanceSquaredTo(ri.location);
+					if (dist < closestAllyDist) {
+						closestAllyDist = dist;
+						closestAllyIndex = index;
+					}
+				}
+				index++;
+			}
+
+			// if there is a nearby enemy that can be picked up, try to chase it
+			if (closestAllyIndex != -1) {
+				MapLocation loc = visibleAllies[closestAllyIndex].location;
+				Direction dir = Nav.tryMoveInDirectionDrone(here.directionTo(loc));
+				Debug.tlog("Moving towards miner at " + loc);
+			}
+		}
+
+	}
+
+	public static void richardMethod() throws GameActionException {
 		if (rc.isCurrentlyHoldingUnit()) {
 			for (Direction dir: directions) {
 				MapLocation loc = rc.adjacentLocation(dir);
