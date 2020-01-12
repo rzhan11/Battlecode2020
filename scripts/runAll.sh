887bca86ddda81f0a7311877b0d7a2ad6e34006c
rzhan11 <richymanzhan@gmail.com>
2020-01-12 12:23:31 -0500

    checks against all

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,2 +1,3 @@
 cd ..
+#./gradlew run -PteamA=kryptonite -PteamB=examplefuncsplayer -Pmaps=ALandDivided > scripts/log.txt
 ./gradlew run -PteamA=kryptonite -PteamB=examplefuncsplayer -Pmaps=ALandDivided,CentralLake,CentralSoup,FourLakeLand,SoupOnTheSide,TwoForOneAndTwoForAll,WaterBot > scripts/log.txt
