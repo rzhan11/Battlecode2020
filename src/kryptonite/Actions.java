30c3cceb1489d64323652696caa547d4fa8e3808
Richard Zhan <richymanzhan@gmail.com>
2020-01-13 12:34:53 -0500

    added dig dirt, deposit dirg, build robot

--- src/kryptonite/Actions.java
+++ src/kryptonite/Actions.java
@@ -9,7 +9,10 @@ public class Actions extends Globals {
 	public static int[] PURPLE = {128, 0, 128}; // dropping robot
 	public static int[] ORANGE = {256, 128, 0}; // shooting drone
 	public static int[] WHITE = {255, 255, 255}; // drone explore symmetry
+	// white also used for dig dirt
 	public static int[] BLACK = {0, 0, 0}; // drone targetting HQLocation
+	// black also used for deposit dirt
+	public static int[] CYAN = {0, 255, 255}; // drone explore symmetry
 
 	private static int[] color;
 
@@ -35,5 +38,23 @@ public class Actions extends Globals {
 		color = ORANGE;
 		rc.setIndicatorLine(here, rc.senseRobot(id).location, color[0], color[1], color[2]);
 		rc.shootUnit(id);
+	}
+
+	public static void doDigDirt (Direction dir) throws GameActionException {
+		color = WHITE;
+		rc.setIndicatorLine(here, rc.adjacentLocation(dir), color[0], color[1], color[2]);
+		rc.digDirt(dir);
+	}
+
+	public static void doDepositDirt (Direction dir) throws GameActionException {
+		color = BLACK;
+		rc.setIndicatorLine(here, rc.adjacentLocation(dir), color[0], color[1], color[2]);
+		rc.depositDirt(dir);
+	}
+
+	public static void doBuildRobot (RobotType type, Direction dir) throws GameActionException {
+		color = CYAN;
+		rc.setIndicatorLine(here, rc.adjacentLocation(dir), color[0], color[1], color[2]);
+		rc.buildRobot(type, dir);
 	}
 }
