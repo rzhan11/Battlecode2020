15a45dd7cec2fbb8b6f11ed6b882a93415684ea6
Richard Zhan <richymanzhan@gmail.com>
2020-01-27 18:38:31 -0500

    Update runAll.sh

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -3,6 +3,9 @@ cd ..
 #./gradlew run -PteamA=kryptonite -PteamB=examplefuncsplayer -Pmaps=ALandDivided,CentralLake,CentralSoup,FourLakeLand,SoupOnTheSide,TwoForOneAndTwoForAll,WaterBot > scripts/log.txt
 
 #./gradlew run -PteamA=kryptonite -PteamB=kryptonite -Pmaps=ALandDivided > scripts/log.txt
-./gradlew run -PteamA=kryptonite -PteamB=kryptonite -Pmaps=CowFarm > scripts/log.txt
+./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=ALandDivided,wall3x3,wall2x2,CowFarm,RandomSoup2,steep > scripts/log.txt
+#./gradlew run -PteamA=rush_bot -PteamB=rush_bot -Pmaps=ALandDivided,wall3x3,wall2x2,CowFarm,RandomSoup2,steep > scripts/log.txt
+# ./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=CowFarm > scripts/log.txt
+#,CentralSoup,CowFarm > scripts/log.txt
 #./gradlew run -PteamA=kryptonite -PteamB=kryptonite -Pmaps=Infinity > scripts/log.txt
 #./gradlew run -PteamA=kryptonite -PteamB=kryptonite -Pmaps=ALandDivided,CentralLake,CentralSoup,FourLakeLand,SoupOnTheSide,TwoForOneAndTwoForAll,WaterBot > scripts/log.txt
