7da4f9f5b909e848322ed34de050a04df2664142
Richard Zhan <richymanzhan@gmail.com>
2020-01-28 23:48:39 -0500

    Spreadsheets :)

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -3,7 +3,8 @@ cd ..
 #./gradlew run -PteamA=kryptonite -PteamB=examplefuncsplayer -Pmaps=ALandDivided,CentralLake,CentralSoup,FourLakeLand,SoupOnTheSide,TwoForOneAndTwoForAll,WaterBot > scripts/log.txt
 
 #./gradlew run -PteamA=kryptonite -PteamB=kryptonite -Pmaps=ALandDivided > scripts/log.txt
-./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=ALandDivided,wall3x3,wall2x2,CowFarm,RandomSoup2,steep > scripts/log.txt
+#./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=ALandDivided,wall3x3,wall2x2,CowFarm,RandomSoup2,steep > scripts/log.txt
+./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=Islands > scripts/log.txt
 #./gradlew run -PteamA=rush_bot -PteamB=rush_bot -Pmaps=ALandDivided,wall3x3,wall2x2,CowFarm,RandomSoup2,steep > scripts/log.txt
 # ./gradlew run -PteamA=rush_bot -PteamB=seeding -Pmaps=CowFarm > scripts/log.txt
 #,CentralSoup,CowFarm > scripts/log.txt
